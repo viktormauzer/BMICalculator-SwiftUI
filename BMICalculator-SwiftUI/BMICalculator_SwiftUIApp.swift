7946a95a6260d5c7f82b00e5a8acdd3fa9b7463e
Viktor Mauzer <mauzer.viktor@icloud.com>
2021-10-12 14:31:16 +0200

    Initial Commit

---
+++ BMICalculator-SwiftUI/BMICalculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMICalculator_SwiftUIApp.swift
+//  BMICalculator-SwiftUI
+//
+//  Created by Viktor Mauzer on 12.10.2021..
+//
+
+import SwiftUI
+
+@main
+struct BMICalculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
